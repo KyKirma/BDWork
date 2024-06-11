f0b13294c1db5e986307cff038f6e072f03697b7
KyKirma <pedrohenriquekourly@hotmail.com>
2024-06-11 10:27:43 -0300

    Flor e categoria update DML

--- DML.sql
+++ DML.sql
@@ -1,23 +1,30 @@
 -- Inserir os dados nas tabelas
 
-INSERT INTO categoria_flor(nome_categoria, descricao_categoria) VALUES (
-    ('Rosa','Ótimas para ocasiões com intenção de demonstrar: paixão, amor, respeito, adoração'),
-    ('Orquidea', 'Brancas'),
-    ('Tulipas', 'Amarela')
-);
+INSERT INTO categoria_flor(nome_categoria, descricao_categoria) VALUES 
+    ('Rosa','Simboliza paixão, amor, respeito, adoração'),
+    ('Orquidea', 'Simboliza amor, admiração e desejo de prosperidade'),
+    ('Tulipas', 'Tulipas simboliza amor, renovação e elegância.'),
+    ('Lírios', 'Lírios simboliza pureza, devoção e novas começos.'),
+    ('Girassol', 'Simboliza alegria, lealdade e energia positiva' );
 
-INSERT INTO cliente(nome_cliente, numero_telefone, email) VALUES (
-    ('', '')
-);
+INSERT INTO cliente(nome_cliente, numero_telefone, email) VALUES 
+    ('', '');
 
-INSERT INTO detalhe_pedido(pedido_id, flor_id, quantidade) VALUES (
-    ('')
-);
 
-INSERT INTO flor(categoria_id, nome_flor, descricao, estoque_disponivel, preco) VALUES (
-    (1, 'Rosas', 'Rosas sortidas')
-);
+INSERT INTO detalhe_pedido(pedido_id, flor_id, quantidade) VALUES
+    ('');
 
-INSERT INTO pedido(cliente_id, status_pedido, valor_total, metodo_pagamento) VALUES (
-    ('')
-);
+
+INSERT INTO flor(categoria_id, nome_flor, descricao, estoque_disponivel, preco) VALUES
+    (1, 'Buquê', 'P Vendida em forma de buquê ou unitária', 35, 100.00),
+    (1, 'Buquê', 'M Vendida em forma de buquê ou unitária', 15, 200.00),
+    (1, 'Buquê', 'G Vendida em forma de buquê ou unitária', 3, 300.00),
+    (2, 'Buquê', 'P Vendida em forma de buquê ou unitária', 24, 100.00),
+    (2, 'Buquê', 'M Vendida em forma de buquê ou unitária', 17, 200.00),
+    (2, 'Buquê', 'G Vendida em forma de buquê ou unitária', 2, 300.00),
+    (3, 'Buquê', 'P Vendida em forma de buquê ou unitária', 13, 150.00),
+    (3, 'Buquê', 'M Vendida em forma de buquê ou unitária', 7, 250.00),
+    (3, 'Buquê', 'G Vendida em forma de buquê ou unitária', 1, 350.00);
+
+INSERT INTO pedido(cliente_id, status_pedido, valor_total, metodo_pagamento) VALUES
+    ('');

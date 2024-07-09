237547a31b913682a1665bb26c0c2b47a6b9ae41
KyKirma <pedrohenriquekourly@hotmail.com>
2024-07-09 09:24:19 -0300

    Update views

--- DDL.sql
+++ DDL.sql
@@ -59,4 +59,35 @@ CREATE TABLE IF NOT EXISTS detalhe_pedido (
     FOREIGN KEY (flor_id) REFERENCES flor(id_flor)
 );
 
+-- Pergunta: Quantos pedidos foram feitos para cada status de pedido?
+CREATE VIEW V_Questao1 AS
+SELECT status_pedido, COUNT(id_pedido) AS total_pedidos
+FROM pedido
+GROUP BY status_pedido;
 
+-- Pergunta: Qual é a média do valor total dos pedidos realizados por mês?
+CREATE VIEW V_Questao2 AS
+SELECT DATE_FORMAT(data, '%Y-%m') AS mes,
+       TRUNCATE(AVG(valor_total), 2) AS media_valor_total 
+FROM pedido 
+WHERE status_pedido = 'Realizado'
+GROUP BY DATE_FORMAT(data, '%Y-%m');
+
+-- Pergunta: Quais são os nomes dos clientes que têm pedidos cancelados?
+CREATE VIEW V_Questao3 AS
+SELECT nome_cliente 
+FROM cliente 
+JOIN pedido ON cliente.id_cliente = pedido.cliente_id 
+WHERE status_pedido = 'Cancelado';
+
+-- Pergunta: Qual é o maior valor entre todos os pedidos e qual é o cliente associado a esse pedido?
+CREATE VIEW V_Questao4 AS
+SELECT p.id_pedido, p.valor_total, c.nome_cliente
+FROM pedido p
+JOIN cliente c ON p.cliente_id = c.id_cliente
+WHERE p.valor_total = (SELECT MAX(valor_total) FROM pedido);
+
+-- Pergunta: Quantas flores existem ao total no estoque?
+CREATE VIEW V_Questao5 AS
+SELECT SUM(estoque_disponivel) AS total_estoque
+FROM flor;
--- Questoes.sql
+++ Questoes.sql
@@ -1,32 +1,31 @@
--- Pergunta: Qual é o total de vendas feitas por cada cliente até o momento, e quem são os cinco principais clientes em termos de valor de compras?
-SELECT cliente_id, SUM(valor) AS total_compras
-FROM vendas
-GROUP BY cliente_id
-ORDER BY total_compras DESC
-LIMIT 5;
+-- Pergunta: Quantos pedidos foram feitos para cada status de pedido?
+SELECT status_pedido, COUNT(id_pedido) AS total_pedidos
+FROM pedido
+GROUP BY status_pedido;
 
--- Pergunta: Qual é o produto mais vendido até agora e quantas unidades foram vendidas?
-SELECT produto_id, nome_produto, SUM(quantidade) AS total_vendido
-FROM vendas
-JOIN produtos ON vendas.produto_id = produtos.id
-GROUP BY produto_id, nome_produto
-ORDER BY total_vendido DESC
-LIMIT 1;
+-- Pergunta: Qual é a média do valor total dos pedidos realizados por mês?
+CREATE VIEW V_Questao2 AS
+SELECT DATE_FORMAT(data, '%Y-%m') AS mes,
+       TRUNCATE(AVG(valor_total), 2) AS media_valor_total 
+FROM pedido 
+WHERE status_pedido = 'Realizado'
+GROUP BY DATE_FORMAT(data, '%Y-%m');
 
--- Pergunta: Qual é o dia da semana com o maior número de vendas até o momento e quantas vendas ocorreram nesse dia?
-SELECT DAYNAME(data_venda) AS dia_semana, COUNT(*) AS total_vendas
-FROM vendas
-GROUP BY dia_semana
-ORDER BY total_vendas DESC
-LIMIT 1;
+-- Pergunta: Quais são os nomes dos clientes que têm pedidos cancelados?
+CREATE VIEW V_Questao3 AS
+SELECT nome_cliente 
+FROM cliente 
+JOIN pedido ON cliente.id_cliente = pedido.cliente_id 
+WHERE status_pedido = 'Cancelado';
 
--- Pergunta: Qual é a média de vendas por mês ao longo do último ano?
-SELECT YEAR(data_venda) AS ano, MONTH(data_venda) AS mes, AVG(valor) AS media_vendas
-FROM vendas
-WHERE data_venda >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 YEAR)
-GROUP BY ano, mes;
+-- Pergunta: Qual é o maior valor entre todos os pedidos e qual é o cliente associado a esse pedido?
+CREATE VIEW V_Questao4 AS
+SELECT p.id_pedido, p.valor_total, c.nome_cliente
+FROM pedido p
+JOIN cliente c ON p.cliente_id = c.id_cliente
+WHERE p.valor_total = (SELECT MAX(valor_total) FROM pedido);
 
--- Pergunta: Quais são os produtos que nunca foram vendidos?
-SELECT id, nome_produto
-FROM produtos
-WHERE id NOT IN (SELECT DISTINCT produto_id FROM vendas);
+-- Pergunta: Quantas flores existem ao total no estoque?
+CREATE VIEW V_Questao5 AS
+SELECT SUM(estoque_disponivel) AS total_estoque
+FROM flor;
